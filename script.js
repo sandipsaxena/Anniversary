e3d1ddf9e67437994ad43d5a95516b690803035b
sandipsaxena <hisaxena@gmail.com>
2025-01-26 10:12:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
 document.getElementById('showWishesButton').addEventListener('click', function() {
-    const wishes = [
-        {
-            "name": "Didi and Jijaji",
-            "wish": "Happy Anniversary to a beautiful couple; may your love continue to blossom!"
-        },
-        {
-            "name": "Brother",
-            "wish": "Happy Anniversary, brother; wishing you endless love and joy!"
-        }
-    ];
-
-    const wishesContainer = document.getElementById('wishesContainer');
-    wishesContainer.innerHTML = wishes.map(wish => `<div class="wish"><p><strong>${wish.name}:</strong> ${wish.wish}</p></div>`).join('');
+    const imagesContainer = document.getElementById('imagesContainer');
+    imagesContainer.classList.toggle('hidden');
 });
